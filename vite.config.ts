67b024cc376364906004276d61c44b318684fe01
Ruttvik21 <rkhollam21@live.com>
2025-11-13 23:46:47 +0530

    fix: package json

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import { hydrogen } from "@shopify/hydrogen/vite";
-import { oxygen } from "@shopify/mini-oxygen/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [hydrogen(), oxygen(), tsconfigPaths()],
+  plugins: [hydrogen(), tsconfigPaths()],
   resolve: {
     alias: {
       "~/": new URL("./app/", import.meta.url).pathname,
